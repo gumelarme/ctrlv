6324df81abde1f25eaf3ed60b502ba7f4a205c6e
gumelarme <gumelar.pn@gmail.com>
2022-04-12 09:54:44 +0800

    Feat: working `New+` button

--- public/assets/script.js
+++ public/assets/script.js
@@ -8,4 +8,28 @@ function attachPostSelectorEvents() {
   }
 }
 
+// Clear form for new post
+function clearForm() {
+  const title = document.querySelector("#editor input[name=Title]");
+  const id = document.querySelector("#editor input[name=Id]");
+  const alias = document.querySelector("#editor input[name=Alias]");
+  const category = document.querySelector("#editor input[name=Category]");
+  const content = document.querySelector("#editor textarea");
+  const visibilityOptions = document.querySelector("#editor select[name=Visibility]").options;
+
+  id.value = "";
+  alias.value = "";
+  content.value = "";
+  title.value = "";
+  category.value = "note";
+  for (const opt of visibilityOptions) {
+    opt.selected = opt.value == "private";
+  }
+}
+
+
 attachPostSelectorEvents()
+const btnNew = document.querySelector("#btn-new");
+btnNew.addEventListener("click", function() {
+  clearForm()
+})
